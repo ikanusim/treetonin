df7b9a9aad2b7b4723b0202e19feec206ca71fbc
Daniel Hüsch <daniel.huesch@gmail.com>
2009-03-02 23:11:08 +0100

    added node temperature feature

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,7 +2,8 @@ class Node < ActiveRecord::Base
   acts_as_tree
 
   def to_hash(deep = false)
-    hash = {'id' => id, 'parent_id' => parent_id, 'name' => name, 'content' => content}
+    min, max = ActiveRecord::Base.connection.select_all('SELECT MIN(updated_at) AS min, MAX(updated_at) AS max FROM nodes').first.values_at('min', 'max').map {|time| Time.parse(time)}
+    hash = {'id' => id, 'parent_id' => parent_id, 'name' => name, 'content' => content, 'red' => (255 * ((updated_at - min) / (max - min))).to_i}
     hash['children'] = children.map {|child| child.to_hash(deep)} if deep && children.present?
     hash
   end
